f2f9ede248f2c770818390fd45829d32aa83d0f1
Lorna Watson <hello@lorna.dev>
2021-03-17 00:10:50 +0000

    able to parse data for chart (to a point)

--- src/app/modules/octopus-deploy/deployments/deployments.component.ts
+++ src/app/modules/octopus-deploy/deployments/deployments.component.ts
@@ -1,8 +1,6 @@
 import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { OctopusDeployApiService } from "../../../../core/services/octopus-deploy.api.service";
-import { map, mapTo } from 'rxjs/operators';
-import { of } from 'rxjs';
 
 @Component({
   selector: 'app-deployments',
@@ -12,14 +10,19 @@ export class DeploymentsComponent implements OnInit {
   rowData: any[];
   totalCount: number;
   count: number;
+  deployedBy: any;
+
+  view: any[] = [900, 400];
 
   gridApi;
   gridColumnApi;
   columnDefs;
   defaultColDef;
 
-  results: any;
-  view: any[] = [700, 400];
+  showLabels = true;
+  explodeSlices = false;
+  doughnut = false;
+  legendTitle = 'Deployed by';
 
   colorScheme = {
     domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
@@ -28,66 +31,16 @@ export class DeploymentsComponent implements OnInit {
   constructor(private service: OctopusDeployApiService,
     private datePipe: DatePipe) { }
 
-
-  groupedData: any;
-
   ngOnInit(): void {
     this.service.getDeployments().subscribe((data: any) => {
       this.rowData = data.Items;
       this.totalCount = data.TotalResults;
       this.count = data.Items.length;
 
-      //const groupBy = (array, key) => {
-      //  return array.reduce((result, currentValue) => {
-      //    (result[currentValue[key]] = result[currentValue[key]] || []).push(currentValue);
-      //    return result;
-      //  }, {});
-      //};
-      const groupBy = key => array =>
-        array.reduce((objectsByKeyValue, obj) => {
-          const value = obj[key];
-          objectsByKeyValue[value] = (objectsByKeyValue[value] || []).concat(obj);
-          return objectsByKeyValue;
-        }, {});
+      this.deployedBy = this.getChartData(data.Items, 'DeployedBy');
 
-
-      const source = groupBy('DeployedBy');
-
-      var k = source(data.Items);
-
-      console.log(k);
-
-
-
-
-
-
-
-      //const myUsers = [
-      //  { name: 'shark', likes: 'ocean' },
-      //  { name: 'turtle', likes: 'pond' },
-      //  { name: 'otter', likes: 'fish biscuits' }
-      //];
-
-      const usersByLikes = data.Items.map(item => {
-        const container = {};
-         
-        container.name = item.Name;  
-
-        return container;
-      });
-
-      console.log(usersByLikes);
-
-      //const example = source.pipe(mapTo(elem => this.groupedData.push({ name: elem.DeployedBy, value: elem.count })));
-      //const subscribe = example.subscribe(val => console.log(val)); 
-
-      //var output = [this.groupedData].map((s, i) => ({ name: s, value: s[i] })); 
+      console.log(data.Items);
     });
-
-    //this.service.getDeploymentChart().subscribe((data: any) => {
-    //  this.results = data.series;
-    //});
 
     this.columnDefs = [
       { field: 'Id', hide: true },
@@ -111,6 +64,27 @@ export class DeploymentsComponent implements OnInit {
       autoHeight: true,
       rowSelection: 'single'
     };
+  }
+
+  getChartData(records, field) {
+    const groupBy = (array, key) => {
+      return array.reduce((result, currentValue) => {
+        (result[currentValue[key]] = result[currentValue[key]] || []).push(currentValue);
+        return result;
+      }, {});
+    };
+
+    const source = groupBy(records, field);
+
+    var groupedData = [];
+
+    for (let prop in source) {
+      if (Object.prototype.hasOwnProperty.call(source, prop)) {
+        groupedData.push({ "name": prop, "value": source[prop].length });
+      }
+    }
+
+    return groupedData;
   }
 
   onGridReady(params) {
